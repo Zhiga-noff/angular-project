54d9b374b1e418594fe5ea5f054a20d8300c01dd
KZHiganov <kzhiganov@zvezdamedia.ru>
2024-01-25 16:10:38 +0300

    Очистил проект

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,12 @@
-import {
-  Component,
-  ComponentFactoryResolver,
-  OnInit,
-  ViewContainerRef,
-} from '@angular/core';
-import { DinamycComponent } from './dinamyc/dinamyc.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'angular';
 
-  constructor(
-    private viewContainer: ViewContainerRef,
-    private componentFactoryResolved: ComponentFactoryResolver,
-  ) {}
-
-  ngOnInit() {
-    setTimeout(() => {
-      const componentFactory =
-        this.componentFactoryResolved.resolveComponentFactory(DinamycComponent);
-      const componentRef = this.viewContainer.createComponent(componentFactory);
-    }, 5000);
-  }
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
-import { UserCardComponent } from './header/user-card/user-card.component';
-import { ItemComponent } from './item/item.component';
-import { ColoryDirective } from './colory.directive';
-import { DelayDirective } from './delay.directive';
-import { DinamycComponent } from './dinamyc/dinamyc.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MenuComponent,
-    UserCardComponent,
-    ItemComponent,
-    ColoryDirective,
-    DelayDirective,
-    DinamycComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
