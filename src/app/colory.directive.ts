51b3f77880d80f3293d3b3e036ceb296b2f7f14a
KZHiganov <kzhiganov@zvezdamedia.ru>
2024-01-25 13:26:53 +0300

    Посмотрел контроллер дерективы

--- src/app/colory.directive.ts
+++ src/app/colory.directive.ts
@@ -1,26 +1,30 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appColory]'
+  selector: '[appColory]',
+  exportAs: 'colory',
 })
 export class ColoryDirective {
-  private counter: number = 0
+  private counter: number = 0;
 
   // @HostBinding() - обновление свойств элемента на который повешана деректива
-  @HostBinding('style.color') myColor: string
+  @HostBinding('style.color') myColor: string;
 
   // @HostListener() - обрабатывает события элемента на который повешана деректива
   @HostListener('click', ['$event']) changeColor(event) {
-    this.myColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
-    this.counter++
-    console.log(this.counter)
+    this.setRand0mColor();
+    this.counter++;
+    console.log(this.counter);
+  }
+
+  setRand0mColor() {
+    this.myColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
   }
 
   constructor() {
-    this.myColor = 'red'
+    this.myColor = 'red';
     setTimeout(() => {
-      this.myColor = 'green'
-    }, 2000)
+      this.myColor = 'green';
+    }, 2000);
   }
-
 }
